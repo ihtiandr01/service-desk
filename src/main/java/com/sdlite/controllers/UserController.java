20a740385c7fe11f0058de35bc3ff8ced722e176
kornevo <olkornev@rambler.ru>
2015-12-28 14:16:41 +0300

    Update templates

--- src/main/java/com/sdlite/controllers/UserController.java
+++ src/main/java/com/sdlite/controllers/UserController.java
@@ -1,11 +1,14 @@
 package com.sdlite.controllers;
 
 
+import com.sdlite.controllers.forms.NewUserForm;
 import com.sdlite.domain.entities.User;
+import com.sdlite.domain.entities.builders.UserBuilder;
 import com.sdlite.domain.repositaries.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -30,6 +33,25 @@ public class UserController {
         model.addAttribute("user",userRepository.findOne(userId));
         return "user";
     }
+
+
+    @RequestMapping(value = "/newuser", method = RequestMethod.GET)
+    public String newUserForm(Model model) {
+        model.addAttribute("newuser", new NewUserForm());
+        return "newuser";
+    }
+
+    @RequestMapping(value = "/newuser", method = RequestMethod.POST)
+    public String submitNewTicket(@ModelAttribute NewUserForm newUserForm, Model model) {
+        User user = UserBuilder.newInstance()
+                .setName(newUserForm.getName())
+                .setPassword(newUserForm.getPassword())
+                .setEmail(newUserForm.getEmail())
+                .build();
+        userRepository.save(user);
+        return "redirect:/users";
+    }
+
 
 
 }
