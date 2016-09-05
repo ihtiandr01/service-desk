02404cbf17e4ab15302e4444d8ba1ade388a5fe4
kornevo <olkornev@rambler.ru>
2016-09-05 14:45:32 +0300

    add localization messages

--- src/main/java/com/sdlite/localization/MessageConfiguration.java
+++ src/main/java/com/sdlite/localization/MessageConfiguration.java
@@ -5,8 +5,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.stereotype.Component;
 
-import java.util.Locale;
-
 @Component
 public class MessageConfiguration {
 
@@ -17,8 +15,6 @@ public class MessageConfiguration {
     messageSource.setUseCodeAsDefaultMessage(true);
     messageSource.setDefaultEncoding("UTF-8");
     messageSource.setBasenames("classpath:messages");
-    String str = messageSource.getMessage("messages.home_ru_RU",null,"empty",new Locale("ru","RU"));
-    str.isEmpty();
     return messageSource;
   }
 
